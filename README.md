6726e54331084edd8f481610de09124ac952d812
cjflan <89868659+cjflan@users.noreply.github.com>
2024-10-29 13:28:33 -0600

    configurable port

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Go webserver
+
+Use this when you need a simple web server to check port availability
+
+Launch using `go run cmd/main.go --port 8080`
--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+    p := flag.String("port", "8080", "port for the web server")
+    flag.Parse()
+
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "hello")
     })
 
-    http.ListenAndServe(":8080", nil)
+    port := fmt.Sprint(":", *p)
+
+    http.ListenAndServe(port, nil)
 }
